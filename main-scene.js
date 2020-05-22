0023b2a0d34c65c0f6b55198f336ef9fb8841bd7
matthewvalley <matthew.valley1@gmail.com>
2020-05-21 17:53:09 -0700

    Adding player object

--- main-scene.js
+++ main-scene.js
@@ -8,18 +8,25 @@ window.Xplore = window.classes.Xplore =
                 context.register_scene_component(new Movement_Controls(context, control_box.parentElement.insertCell()));
 
             const r = context.width / context.height;
-            context.globals.graphics_state.camera_transform = Mat4.translation([0, -3, 0]);  // Locate the camera here (inverted matrix).
+            context.globals.graphics_state.camera_transform = Mat4.translation([0, -10, -10]);  // Locate the camera here (inverted matrix).
             context.globals.graphics_state.projection_transform = Mat4.perspective(Math.PI / 4, r, .1, 1000);
+
+            this.player = new Player(context.globals.graphics_state);
 
             const shapes = {
                 'box': new Cube(),
                 'ground': new Ground(),
                 'triangle': new Triangle(),
-                'pyramid': new Pyramid()
+                'pyramid': new Pyramid(),
+                'player': this.player.model
             };
 
             this.materials = {
                 grass: context.get_instance(Phong_Shader).material(Color.of(0, 1, 0, 1), {
+                    ambient: .4,
+                    diffusivity: .4
+                }),
+                sky: context.get_instance(Phong_Shader).material(Color.of(1, 1,1, 1), {
                     ambient: .4,
                     diffusivity: .4
                 }),
@@ -48,8 +55,9 @@ window.Xplore = window.classes.Xplore =
             this.plastic = this.clay.override({specularity: .6});
             this.grass_texture = this.materials.grass.override({texture: context.get_instance("assets/grass.jpg")});
             this.stars = this.plastic.override({texture: context.get_instance('assets/stars.png')})
+            this.sun = this.materials.sky.override({texture: context.get_instance('assets/sun.png')})
 
-            this.lights = [new Light(Vec.of(0, 5, 5, 1), Color.of(1, .4, 1, 1), 100000)];
+            this.lights = [new Light(Vec.of(0, 100, 5, 1), Color.of(1, .4, 1, 1), 100000)];
 
             this.randomX =  [...Array(100)].map(() => Math.floor(300*Math.random() + -150));
             this.randomZ =  [...Array(100)].map(() => Math.floor(300*Math.random() + -370));
@@ -64,10 +72,10 @@ window.Xplore = window.classes.Xplore =
 
         }
 
-        drawGround() {
-            let model_transform = Mat4.identity();
-            model_transform = model_transform.times(Mat4.scale([300, 1, 300]));
-            this.shapes.ground.draw(this.globals.graphics_state, model_transform, this.grass_texture);
+        drawGround(x, y, z, size, texture) {
+            let model_transform = Mat4.identity().times(Mat4.translation([x,y,z]));
+            model_transform = model_transform.times(Mat4.scale([size, 1, size]));
+            this.shapes.ground.draw(this.globals.graphics_state, model_transform, texture);
         }
 
         drawTree(x, z, height) {
@@ -75,16 +83,16 @@ window.Xplore = window.classes.Xplore =
 
             let trunk_transform = loc.times(Mat4.scale([1,height,1]));
 
-            let leaves_bottom_transform = loc.times(Mat4.translation([0,.8*height,0]))
-                                            .times(Mat4.scale([1.5*height,1*height,1.5*height]));
+            let leaves_bottom_transform = loc.times(Mat4.translation([0,.2*height,0]))
+                                            .times(Mat4.scale([.3*height,.8*height,.3*height]));
 
-            let leaves_middle_transform = loc.times(Mat4.translation([0,1.2*height,0]))
-                                            .times(Mat4.scale([1.3*height,1*height,1.3*height]));
+            let leaves_middle_transform = loc.times(Mat4.translation([0,.4*height,0]))
+                                            .times(Mat4.scale([.3*height,.6*height,.3*height]));
 
-            let leaves_top_transform = loc.times(Mat4.translation([0,1.6*height,0]))
-                                        .times(Mat4.scale([1*height,1*height,1*height]));
+            let leaves_top_transform = loc.times(Mat4.translation([0,.6*height,0]))
+                                        .times(Mat4.scale([.3*height,.4*height,.3*height]));
 
-            this.shapes.box.draw(this.globals.graphics_state, trunk_transform, this.materials.bark);
+            this.shapes.pyramid.draw(this.globals.graphics_state, trunk_transform, this.materials.bark);
             this.shapes.pyramid.draw(this.globals.graphics_state, leaves_bottom_transform, this.materials.green1);
             this.shapes.pyramid.draw(this.globals.graphics_state, leaves_middle_transform, this.materials.green2);
             this.shapes.pyramid.draw(this.globals.graphics_state, leaves_top_transform, this.materials.green3);
@@ -114,9 +122,15 @@ window.Xplore = window.classes.Xplore =
         display(graphics_state) {
             graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
 
-            this.drawGround();
+            this.drawGround(0, 0, 0, 1000, this.grass_texture);
+            this.drawGround(0, 100, 0, 1000, this.sun);
 
-            this.drawForest();
+
+            //this.drawForest();
+
+            this.player.translateLoc(graphics_state.player_transform)
+            this.player.model.draw(graphics_state, this.player.loc, this.plastic)
+
         }
 
     };
