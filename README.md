b4399c9e7c53f71d7c1e4a4b6c2534fdd7406021
Akram Sabbah <akramsabbah9@gmail.com>
2020-06-05 23:45:08 -0700

    last minute touch ups

--- README.md
+++ README.md
@@ -37,6 +37,8 @@ For Mac run host.command then go to localhost:8000 in the browser of your choice
 
 Collision Detection: Implemented to keep the player inside the game domain, and to keep it out of certain structures, such as trees, cacti and the pyramid. Collision detection was also used in the last level: if the player touched the lava floor, they would respawn at the beginning of the lava, and if they touched a purple obelisk, they would spawn a new platform to walk on.
 
+Shadowing: The trees in Level One cast shadows on the floor. Created using Jonathan Mitchell's shadow shader.
+
 Particle Simulation: Designed player-tracked snow particles to give the illusion of snowfall. Rather than drawing snow objects across the entire level, localizing the snowfall to the player location notably optimized runtime performance. Snow particles are first drawn at a set height and accelerated towards the ground. When a particle hits the floor, it is redrawn at its initial height again. This is then enhanced by adding several more particles to the operation. This loop of actions creates a seamless illusion of global snow.
 
 Mouse Controls: To properly implement Xplore’s controls, the default Movement_Controls (in dependencies.js) were removed. The commands were re-written from the ground up to allow for camera rotation without any z-axis skew. This allowed the orientation of the camera matrix to stay aligned with the level. Javascript event listeners were also added to allow for click-and-drag mouse rotation. Our implementation only rotates the camera when the mouse is dragged in a direction, unlike the default Movement_Controls which rotates the camera as long as the mouse is slightly off-center.
--- main-scene.js
+++ main-scene.js
@@ -64,6 +64,10 @@ window.Xplore = window.classes.Xplore =
                     diffusivity: .3,
                     texture: context.get_instance("assets/waterBorder.jpg")
                 }),
+                shadow: context.get_instance(Shadow_Shader).material(Color.of(0,0,0,1),
+			        {ambient: 1.0, diffusivity: 0.0, specularity: 0.0 }),
+                    //.override({texture:context.get_instance("models/"+name, true)
+
                 grey: context.get_instance(Phong_Shader).material(Color.of(0.498, 0.5137, 0.5176, 1), {ambient: 0.5}),
 
                 white: context.get_instance(Phong_Shader).material(Color.of(0, 0, 0, 1), {ambient: 1}),
@@ -204,9 +208,13 @@ window.Xplore = window.classes.Xplore =
                                         .times(Mat4.scale([1*height,1*height,1*height]));
 
             this.shapes.box.draw(this.globals.graphics_state, trunk_transform, this.textures.bark);
+            this.shapes.box.draw(this.globals.graphics_state, trunk_transform, this.materials.shadow);
             this.shapes.pyramid.draw(this.globals.graphics_state, leaves_bottom_transform, this.materials.green1);
+            this.shapes.pyramid.draw(this.globals.graphics_state, leaves_bottom_transform, this.materials.shadow);
             this.shapes.pyramid.draw(this.globals.graphics_state, leaves_middle_transform, this.materials.green2);
+            this.shapes.pyramid.draw(this.globals.graphics_state, leaves_middle_transform, this.materials.shadow);
             this.shapes.pyramid.draw(this.globals.graphics_state, leaves_top_transform, this.materials.green3);
+            this.shapes.pyramid.draw(this.globals.graphics_state, leaves_top_transform, this.materials.shadow);
 
         }
 
@@ -919,4 +927,100 @@ class Button {
         this.x = x;
         this.y = y;
     }
+};
+
+// credit to Jonathan Mitchell:
+// https://github.com/JonathanCMitchell/CS174A_DrivingSimulator/blob/master/shadow_shader.js
+class Shadow_Shader extends Phong_Shader {
+	vertex_glsl_code() {
+		return `
+			precision mediump float;
+			attribute vec3 object_space_pos;
+			attribute vec3 normal;
+			attribute vec2 tex_coord;
+			uniform mat4 camera_transform;
+			uniform mat4 camera_model_transform;
+			uniform mat4 projection_camera_model_transform;
+			uniform mat3 inverse_transpose_modelview;
+			mat4 inverse(mat4 a) {
+				mat4 b;
+				b[0][0] = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+				b[0][1] = a[0][0] * a[1][2] - a[0][2] * a[1][0];
+				b[0][2] = a[0][0] * a[1][3] - a[0][3] * a[1][0];
+				b[1][0] = a[0][1] * a[1][2] - a[0][2] * a[1][1];
+				b[1][1] = a[0][1] * a[1][3] - a[0][3] * a[1][1];
+				b[1][2] = a[0][2] * a[1][3] - a[0][3] * a[1][2];
+				b[2][0] = a[2][0] * a[3][1] - a[2][1] * a[3][0];
+				b[2][1] = a[2][0] * a[3][2] - a[2][2] * a[3][0];
+				b[2][2] = a[2][0] * a[3][3] - a[2][3] * a[3][0];
+				b[3][0] = a[2][1] * a[3][2] - a[2][2] * a[3][1];
+				b[3][1] = a[2][1] * a[3][3] - a[2][3] * a[3][1];
+				b[3][2] = a[2][2] * a[3][3] - a[2][3] * a[3][2];
+				b[3][3] = b[0][0] * b[3][2] - b[0][1] * b[3][1] 
+						+ b[0][2] * b[3][0] + b[1][0] * b[2][2]
+						- b[1][1] * b[2][1] + b[1][2] * b[2][0];
+				return mat4(
+					a[1][1] * b[3][2] - a[1][2] * b[3][1] + a[1][3] * b[3][0],
+					a[0][2] * b[3][1] - a[0][1] * b[3][2] - a[0][3] * b[3][0],
+					a[3][1] * b[1][2] - a[3][2] * b[1][1] + a[3][3] * b[1][0],
+					a[2][2] * b[1][1] - a[2][1] * b[1][2] - a[2][3] * b[1][0],
+					a[1][2] * b[2][2] - a[1][0] * b[3][2] - a[1][3] * b[2][1],
+					a[0][0] * b[3][2] - a[0][2] * b[2][2] + a[0][3] * b[2][1],
+					a[3][2] * b[0][2] - a[3][0] * b[1][2] - a[3][3] * b[0][1],
+					a[2][0] * b[1][2] - a[2][2] * b[0][2] + a[2][3] * b[0][1],
+					a[1][0] * b[3][1] - a[1][1] * b[2][2] + a[1][3] * b[2][0],
+					a[0][1] * b[2][2] - a[0][0] * b[3][1] - a[0][3] * b[2][0],
+					a[3][0] * b[1][1] - a[3][1] * b[0][2] + a[3][3] * b[0][0],
+					a[2][1] * b[0][2] - a[2][0] * b[1][1] - a[2][3] * b[0][0],
+					a[1][1] * b[2][1] - a[1][0] * b[3][0] - a[1][2] * b[2][0],
+					a[0][0] * b[3][0] - a[0][1] * b[2][1] + a[0][2] * b[2][0],
+					a[3][1] * b[0][1] - a[3][0] * b[1][0] - a[3][2] * b[0][0],
+					a[2][0] * b[1][0] - a[2][1] * b[0][1] + a[2][2] * b[0][0]
+				) / b[3][3];
+			}
+            void main(void) {
+				mat4 projection = projection_camera_model_transform * inverse(camera_model_transform);
+				mat4 cameraview = camera_transform;
+				mat4 modelworld = inverse(camera_transform) * camera_model_transform;
+				vec4 m = lightPosition[0];
+				vec4 g = vec4(0.0, 1.02, 0.0, 1.0);;
+				mat4 groundTranslation = mat4(
+					1.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, 0.0, 0.0,
+					0.0, 0.0, 1.0, 0.0,
+					m.x,m.y+g.y,m.z,m.w
+				);
+				mat4 shadowProjection = mat4(
+					1.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, 0.0, -1.0 / m.y,
+					0.0, 0.0, 1.0, 0.0,
+					0.0, 0.0, 0.0, 0.0
+				);
+				mat4 lightTranslation = mat4(
+					1.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, 0.0, 0.0,
+					0.0, 0.0, 1.0, 0.0,
+					-m.x,-m.y-g.y,-m.z,m.w
+				);
+				gl_Position
+				= projection
+				* cameraview
+				* groundTranslation
+				* shadowProjection
+				* lightTranslation
+				* modelworld
+				* vec4(object_space_pos, 1.0);
+				f_tex_coord = tex_coord;
+            }
+		`;
+	}
+	fragment_glsl_code() {
+		return `
+            precision mediump float;
+			uniform sampler2D texture;
+            void main(void) {
+                gl_FragColor = vec4(0.0, 0.0, 0.0, 0.8);
+            }
+		`;
+	}
 };
